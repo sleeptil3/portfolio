aa960af492234f70503f967eaa06922be0900a64
sclary110 <sclary@icloud.com>
2021-05-20 14:54:09 -0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+	purge: {
+		enabled: false,
+		content: [
+			'./src/**.js',
+			'./src/**/*.js',
+			'./src/**/**/*.js',
+			'./public/index.html'
+		],
+	},
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		fontFamily: {
+			// 'redHat': ['"Red Hat Display"', 'sans-serif'],
+			// 'mono': ['Roboto Mono', 'monospace']
+		},
+		fontWeight: {
+			// thin: 400,
+			// light: 400,
+			// normal: 500,
+			// medium: 500,
+			// semibold: 500,
+			// bold: 700,
+			// extrabold: 700,
+			// black: 900,
+		},
+		extend: {
+			colors: {
+				// 'darkBlue': '#0B114A',
+			}
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+}
