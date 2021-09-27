a3f457e3812252be6880b05b571e295ddd5e89de
Shawn Clary <sclary@icloud.com>
2021-09-27 11:32:04 -0400

    updated size of font on euchrenight support

--- src/Pages/EuchreNightSupport.js
+++ src/Pages/EuchreNightSupport.js
@@ -19,8 +19,8 @@ export default function EuchreNightSupport() {
 					className="flex flex-col relative px-4 sm:ml-20 mr-10 sm:pl-20 z-10 font-medium text-lg sm:text-xl md:text-2xl space-y-4"
 				>
 					<div className="">
-						<p className="font-normal text-sm"><a href="mailto:euchrenightissues@gmail.com?&subject=[ISSUE%20TITLE]&body=(please%20replace%20the%20subject%20line%20with%20an%20issue%20title)%0D%0A%0D%0A%0ADescription%20of%20Bug%20or%20Feature%20Request:%0D%0A%0D%0A%0D%0A%0D%0A%0ASteps%20to%20Reproduce,%20if%20applicable:%0D%0A%0D%0A%0D%0A%0D%0A%0ADevice%20Model:%20[ex:%20'iPhone%2012%20Pro%20Max']%0D%0A%0D%0A%0D%0A%0D%0A%0A(attach%20any%20screenshots,%20if%20applicable)">Use This Link</a> to submit a bug or feature request.</p>
-						<p className="font-normal text-xs mt-1 italic">If you have a GitHub account, submit an issue <a className="text-xs" href="https://github.com/sleeptil3/euchre-native/issues/new/choose" target="_blank" rel="noreferrer">here</a>.</p>
+						<p className="font-normal text-base"><a href="mailto:euchrenightissues@gmail.com?&subject=[ISSUE%20TITLE]&body=(please%20replace%20the%20subject%20line%20with%20an%20issue%20title)%0D%0A%0D%0A%0ADescription%20of%20Bug%20or%20Feature%20Request:%0D%0A%0D%0A%0D%0A%0D%0A%0ASteps%20to%20Reproduce,%20if%20applicable:%0D%0A%0D%0A%0D%0A%0D%0A%0ADevice%20Model:%20[ex:%20'iPhone%2012%20Pro%20Max']%0D%0A%0D%0A%0D%0A%0D%0A%0A(attach%20any%20screenshots,%20if%20applicable)">Use This Link</a> to submit a bug or feature request.</p>
+						<p className="font-normal text-sm mt-1 italic">If you have a GitHub account, submit an issue <a className="text-xs" href="https://github.com/sleeptil3/euchre-native/issues/new/choose" target="_blank" rel="noreferrer">here</a>.</p>
 					</div>
 					<div className="font-normal text-xs leading-normal">
 						<p className="text-gray-700">
